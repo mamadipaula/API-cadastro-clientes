8449c934a54462a8ecb31ae207907d1e07fd12e8
Marcela <mamadipaula@gmail.com>
2023-03-28 17:39:06 -0300

    SwaggerDoc

--- src/app.ts
+++ src/app.ts
@@ -5,27 +5,30 @@ import { handleErrors } from "./errors";
 import { sessionRoutes } from "./routes/session.routes";
 import { userRoutes } from "./routes/user.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
-import cors from "cors";
+// import cors from "cors";
+import swaggerUI from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
 
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    allowedHeaders: [
-      "sessionId",
-      "Content-Type",
-      "Authorization",
-      "authorization",
-    ],
-    origin: ["http://localhost:3000"],
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-  })
-);
+// app.use(
+//   cors({
+//     allowedHeaders: [
+//       "sessionId",
+//       "Content-Type",
+//       "Authorization",
+//       "authorization",
+//     ],
+//     origin: ["http://localhost:3000"],
+//     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+//   })
+// );
 
 app.use("/users", userRoutes);
 app.use("/login", sessionRoutes);
 app.use("/contacts", contactsRoutes);
+app.use("/doc", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(handleErrors);
 
